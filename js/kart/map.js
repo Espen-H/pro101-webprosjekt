f67d5237a036a30fdd8cd52a1104d4c14ff8d043
Andersmidtbo <andersmid@gmail.com>
2018-05-30 14:11:45 +0200

    Fra min posisjon (IGJEN)

--- js/kart/map.js
+++ js/kart/map.js
@@ -95,6 +95,34 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 }
 
 
+if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  console.log("Geolocation not supported by browser :))");
+}
+
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+   $('#start').append($('<option>', {
+     value: lat + "," + lng,
+     text: 'Min posisjon',
+     style: 'background-color: #17a2b8; color: white'
+  }));
+  var marker = new google.maps.Marker({
+    position: {
+      lat: lat,
+      lng: lng
+      },
+      icon: bikeIcon,
+      label: {
+        text: 'DIN POSISJON',
+        color: 'red',
+        },
+      map: map
+      });
+}
+
 
 function getTravelMode() {
   let radioWalk = document.getElementById("radioWalk");
